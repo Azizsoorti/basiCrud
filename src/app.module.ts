bea8f60b3b45e95c5b16b27ffd69288cdfde0b9a
AzizSoorti <hies.aziz2019@gmail.com>
2023-06-05 18:30:38 +0000

    ten crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,16 @@ import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginEntity } from './login/entities/login.entity';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { CustomerModule } from './customer/customer.module';
+import { ReviewModule } from './review/review.module';
+import { ArticleModule } from './article/article.module';
+import { CommentModule } from './comment/comment.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { SignupModule } from './signup/signup.module';
 
 @Module({
   imports: [ TypeOrmModule.forRoot({
@@ -16,7 +26,17 @@ import { LoginEntity } from './login/entities/login.entity';
     entities: [LoginEntity],
     synchronize: true,
   }),
-   LoginModule],
+   LoginModule,
+   ProductModule,
+   CategoryModule,
+   OrderModule,
+   CustomerModule,
+   ReviewModule,
+   ArticleModule,
+   CommentModule,
+   UserModule,
+   AdminModule,
+   SignupModule],
   controllers: [AppController],
   providers: [AppService],
 })
